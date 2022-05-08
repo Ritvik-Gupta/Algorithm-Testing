8ec72ad740646f8a02300a9ec88ed03b92762f04
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-05-08 17:06:06 +0530

    potd using buffered channels as a generator function

---
+++ leetcode/medium/flatten_nested_list_iterator/main.go
@@ -0,0 +1,51 @@
+package medium
+
+type NestedInteger struct {
+}
+
+func (nestedInteger NestedInteger) IsInteger() bool {
+	panic("")
+}
+func (nestedInteger NestedInteger) GetInteger() int {
+	panic("")
+}
+func (nestedInteger NestedInteger) GetList() []*NestedInteger {
+	panic("")
+}
+
+type NestedIterator struct {
+	generatorChannel  chan int
+	hasNextGeneration bool
+}
+
+func flattenNestingGenerator(nestedList []*NestedInteger, generatorChannel chan int, hasNextGen *bool) {
+	for _, nestedInteger := range nestedList {
+		if nestedInteger.IsInteger() {
+			generatorChannel <- nestedInteger.GetInteger()
+		} else {
+			flattenNestingGenerator(nestedInteger.GetList(), generatorChannel, new(bool))
+		}
+	}
+	*hasNextGen = false
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	obj := NestedIterator{generatorChannel: make(chan int, 1), hasNextGeneration: true}
+	go flattenNestingGenerator(nestedList, obj.generatorChannel, &obj.hasNextGeneration)
+	return &obj
+}
+
+func (nestedIterator *NestedIterator) Next() int {
+	for {
+		select {
+		case val := <-nestedIterator.generatorChannel:
+			return val
+		default:
+			continue
+		}
+	}
+}
+
+func (nestedIterator *NestedIterator) HasNext() bool {
+	return nestedIterator.hasNextGeneration
+}
